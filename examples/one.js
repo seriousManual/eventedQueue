60f4a3f44d53c869e907ef9d08b25d48e93cd90e
zaphod1984 <zaphod84@gmx.de>
2013-06-24 17:13:14 +0200

    examples

--- examples/one.js
+++ examples/one.js
@@ -1,12 +1,29 @@
 var eq = require('../');
 
-var myEq = eq();
+var myEq = eq(true);
 
 myEq.push(function() {
-    console.log( 'ola' );
+    console.log( 'ola0' );
+});
+
+myEq.push(function() {
+    console.log( 'ola1' );
+});
+
+myEq.push(function() {
+    console.log( 'ola2' );
 });
 
 setTimeout(function() {
     myEq.trigger();
 }, 1000);
 
+setTimeout(function() {
+    myEq.push(function() {
+        console.log( 'ola3' );
+    });
+}, 2000);
+
+setTimeout(function() {
+    myEq.trigger();
+}, 2500);
