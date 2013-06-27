32eb2148dfa0372b95a1bb9d13d4c2ad30fc6428
zaphod1984 <zaphod84@gmx.de>
2013-06-27 16:00:44 +0200

    tests

--- tests/test.js
+++ tests/test.js
@@ -133,122 +133,98 @@ describe('eventedQueue', function() {
             }, 100);
         }, 50);
     });
-});
 
-//module.exports['eventedQueue autorelock'] = function(test) {
-//
-//    var callOrder = []
-//        , numberCalled = 0
-//        , eq = eventedQueue(true); //<--- setting default behaviour to relock after trigger
-//
-//    var testFunction = function(nr, time) {
-//        setTimeout(function() {
-//            callOrder.push(nr);
-//
-//            numberCalled++;
-//
-//            if (numberCalled == 4) {
-//                test.same(callOrder, ['task2', 'task1', 'task4', 'task3']);
-//                test.equal(eq.length(), 2);
-//                test.equal(numberCalled, 4);
-//
-//                setTimeout(function() {
-//                    test.equal(eq.length(), 2);
-//                    test.equal(numberCalled, 4);
-//
-//                    eq.trigger();
-//                }, 50);
-//            }
-//
-//            if (numberCalled == 6) {
-//                test.same(callOrder, ['task2', 'task1', 'task4', 'task3', 'task5', 'task6']);
-//                test.equal(eq.length(), 0);
-//                test.equal(numberCalled, 6);
-//                test.done();
-//            }
-//
-//        }, time);
-//    };
-//
-//    eq.push(testFunction, 'task1', 20);
-//    eq.push(testFunction, 'task2', 10);
-//    eq.push(testFunction, 'task3', 40);
-//    eq.push(testFunction, 'task4', 30);
-//
-//    test.equal(eq.length(), 4);
-//    test.equal(numberCalled, 0);
-//
-//    eq.trigger();
-//
-//    eq.push(testFunction, 'task5', 10);
-//    eq.push(testFunction, 'task6', 20);
-//
-//};
-//
-//module.exports['eventedQueue error'] = function(test) {
-//
-//    var eq = async.eventedQueue(false);
-//
-//    test.throws(function() {
-//        eq.push()
-//    });
-//    test.throws(function() {
-//        eq.push(true)
-//    });
-//    test.throws(function() {
-//        eq.push('')
-//    });
-//    test.throws(function() {
-//        eq.push({})
-//    });
-//    test.throws(function() {
-//        eq.push([])
-//    });
-//
-//    test.done();
-//};
-//
-//module.exports['eventedQueue events'] = function(test) {
-//    var callOrder = []
-//        , numberCalled = 0
-//        , eq = eventedQueue(true);
-//
-//    var testFunction = function(nr, time) {
-//        setTimeout(function() {
-//            callOrder.push(nr);
-//
-//            numberCalled++;
-//            if (numberCalled == 4) {
-//                test.same(callOrder, ['empty', 'task2', 'task1', 'task4', 'task3']);
-//                test.equal(eq.length(), 2);
-//                test.equal(numberCalled, 4);
-//
-//                eq.trigger();
-//            }
-//
-//            if (numberCalled == 6) {
-//                test.same(callOrder, ['empty', 'task2', 'task1', 'task4', 'task3', 'empty', 'task5', 'task6']);
-//                test.equal(eq.length(), 0);
-//                test.equal(numberCalled, 6);
-//                test.done();
-//            }
-//        }, time);
-//    };
-//
-//    eq.empty = function() {
-//        callOrder.push('empty');
-//    };
-//
-//    eq.push(testFunction, 'task1', 20);
-//    eq.push(testFunction, 'task2', 10);
-//    eq.push(testFunction, 'task3', 50);
-//    eq.push(testFunction, 'task4', 30);
-//
-//    test.equal(eq.length(), 4);
-//    test.equal(numberCalled, 0);
-//
-//    eq.trigger();
-//
-//    eq.push(testFunction, 'task5', 100);
-//    eq.push(testFunction, 'task6', 120);
-//};
+    it('should relock the queue automatically', function(done) {
+        var callOrder = [];
+        var numberCalled = 0;
+        var eq = eventedQueue(true); //<--- setting default behaviour to relock after trigger
+
+        var testFunction = function(nr, time) {
+            setTimeout(function() {
+                callOrder.push(nr);
+
+                numberCalled++;
+            }, time);
+        };
+
+        eq.push(testFunction, 'task1', 20);
+        eq.push(testFunction, 'task2', 10);
+        eq.push(testFunction, 'task3', 40);
+        eq.push(testFunction, 'task4', 30);
+
+        expect(eq.length()).to.equal(4);
+        expect(numberCalled).to.equal(0);
+
+        eq.trigger();
+
+        setTimeout(function() {
+            expect(eq.length()).to.equal(0);
+            expect(numberCalled).to.equal(4);
+            expect(callOrder).to.deep.equal(['task2', 'task1', 'task4', 'task3']);
+
+            eq.push(testFunction, 'task5', 10);
+            eq.push(testFunction, 'task6', 20);
+
+            expect(eq.length()).to.equal(2);
+            expect(numberCalled).to.equal(4);
+            expect(callOrder).to.deep.equal(['task2', 'task1', 'task4', 'task3']);
+
+            done();
+        }, 80);
+
+    });
+
+    it('should call empty', function(done) {
+        var callOrder = [];
+        var numberCalled = 0;
+        var eq = eventedQueue(); //<--- setting default behaviour to relock after trigger
+
+        eq.empty = function() {
+            done();
+        };
+
+        var testFunction = function(nr, time) {
+            setTimeout(function() {
+                callOrder.push(nr);
+
+                numberCalled++;
+            }, time);
+        };
+
+        eq.push(testFunction, 'task1', 20);
+        eq.push(testFunction, 'task2', 10);
+        eq.push(testFunction, 'task3', 40);
+        eq.push(testFunction, 'task4', 30);
+
+        expect(eq.length()).to.equal(4);
+        expect(numberCalled).to.equal(0);
+
+        setTimeout(function() {
+            eq.trigger();
+        }, 20);
+    });
+
+    it('should throw', function() {
+        var eq = eventedQueue();
+
+        expect(function() {
+            eq.push()
+        }).to.throw();
+
+        expect(function() {
+            eq.push(true)
+        }).to.throw();
+
+        expect(function() {
+            eq.push('')
+        }).to.throw();
+
+        expect(function() {
+            eq.push({})
+        }).to.throw();
+
+        expect(function() {
+            eq.push([])
+        }).to.throw();
+    });
+});
