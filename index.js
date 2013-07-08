9a9decea58453569a9be8e8dc28bff684a0915b2
zaphod1984 <zaphod84@gmx.de>
2013-07-08 09:40:59 +0200

    documentation

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
+/**
+ * a queue that queues up event handlers that get triggered on a certain event
+ * @param autoRelock specifies if the queue should lock itself after triggering and after having invoked all handlers
+ * @constructor
+ */
 function EventedQueue (autoRelock) {
     this._autoRelock = !!autoRelock;
     this._triggered = false;
     this._triggerQueue = [];
 }
 
+/**
+ * noop function that acts as the empty callback when the eventedQueue has emptied
+ */
 EventedQueue.prototype.empty = function() {};
 
+/**
+ * adds an event handler.
+ * first argument has to be a function, the following parameters will be applied on invocation
+ */
 EventedQueue.prototype.push = function() {
     var args = Array.prototype.slice.call(arguments);
 
@@ -24,6 +36,9 @@ EventedQueue.prototype.push = function() {
     }
 };
 
+/**
+ * cause all queued event handlers to be triggered
+ */
 EventedQueue.prototype.trigger = function() {
     this._triggered = true;
 
@@ -39,10 +54,17 @@ EventedQueue.prototype.trigger = function() {
     this.empty && 'function' === typeof this.empty && this.empty();
 };
 
+/**
+ * locks the event queue
+ */
 EventedQueue.prototype.relock = function() {
     this._triggered = false;
 };
 
+/**
+ * returns the size of the event queue
+ * @returns {Number}
+ */
 EventedQueue.prototype.size = function() {
     return this._triggerQueue.length;
 };
